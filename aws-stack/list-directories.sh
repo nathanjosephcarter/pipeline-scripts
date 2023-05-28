152d04ea5fc8fceece339f64660a7c97211af742
Nathan <contact@njc.digital>
2023-05-28 15:56:00 +0100

    debug list-directories.sh

--- aws-stack/list-directories.sh
+++ aws-stack/list-directories.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Capture the list of directories
-directories=$(find . -type d -not -path '*/\.*' -not -path '.' -not -path './.git*' -mindepth 2 -maxdepth 2)
+directories=$(find . -type d -not -path '*/\.*' -not -path '.' -not -path './.git*' -not -path './pipeline-scripts*' -mindepth 2 -maxdepth 2)
 
 # Output a space-separated list
 echo "directories=$(echo $directories | tr '\n' ' ')" | tee $GITHUB_OUTPUT
