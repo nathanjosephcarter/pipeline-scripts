7f0ecabd87db61c7d929a6a66cee6bdaf5b9de24
Nathan <contact@njc.digital>
2023-05-28 20:27:26 +0100

    debug find_modified_directories.sh

--- aws-stack/find_modified_directories.sh
+++ aws-stack/find_modified_directories.sh
@@ -5,11 +5,11 @@ changed_directories=()
 git fetch origin $target_branch
 
 for dir in $directories; do
-git_diff_output=$(git diff origin/$target_branch --name-only -- "$dir")
-if [[ -n "$git_diff_output" ]]; then
+  git_diff_output=$(git diff origin/$target_branch --name-only -- "$dir")
+  if [[ -n "$git_diff_output" ]]; then
     echo "File changes detected in $dir."
     changed_directories+=("$dir")
-else
+  else
     echo "No file changes detected in $dir."
-fi
+  fi
 done
