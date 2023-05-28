9803c79dbbf702bfdf026c83b1375c2fd8d06e95
Nathan <contact@njc.digital>
2023-05-28 21:39:40 +0100

    debug find_modified_directories.sh

--- aws-stack/find_modified_directories.sh
+++ aws-stack/find_modified_directories.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Source the output functions file
+source "./pipeline-scripts/github/set_multiline_github_output.sh"
+
 IFS=$'\n' read -r -d '' -a directories <<< "$DIRECTORIES"
 
 git fetch origin "$TARGET_BRANCH"
 
 modified_directories=()
 for dir in "${directories[@]}"; do
-  echo "Checking $dir"
   git_diff_output=$(git diff "origin/$TARGET_BRANCH" --name-only -- "$dir")
   if [[ -n "$git_diff_output" ]]; then
     echo "File changes detected in $dir."
@@ -15,3 +17,5 @@ for dir in "${directories[@]}"; do
     echo "No file changes detected in $dir."
   fi
 done
+
+set_multiline_github_output -n "modified_directories" -v "$modified_directories"
