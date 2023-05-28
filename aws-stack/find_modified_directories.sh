551bd7f512c6d2a47537c0acdff97b0af305d1ff
Nathan <contact@njc.digital>
2023-05-28 21:00:30 +0100

    debug find_modified_directories.sh

--- aws-stack/find_modified_directories.sh
+++ aws-stack/find_modified_directories.sh
@@ -5,6 +5,7 @@ git fetch origin $target_branch
 changed_directories=()
 while IFS= read -r dir; do
   echo $dir
+  echo $target_branch
   git_diff_output=$(git diff "origin/$target_branch" --name-only -- "./prod/spotify-automation-tasks")
   if [[ -n "$git_diff_output" ]]; then
     echo "File changes detected in $dir."
