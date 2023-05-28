8d9099dce7663fbd43179579c1f196355f2803f3
Nathan <contact@njc.digital>
2023-05-28 14:45:14 +0100

    update list-directories.sh

--- aws-stack/list-directories.sh
+++ aws-stack/list-directories.sh
@@ -1,2 +1,2 @@
 directories=$(find . -type d -not -path '*/\.*' -not -path '.' -not -path './.git*' -mindepth 2 -maxdepth 2)
-echo "directories=$directories" >> $GITHUB_OUTPUT
+echo "directories=$directories" | tee $GITHUB_OUTPUT
