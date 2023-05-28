06cbecfb8f6eebb8e0f90dd1b09486e401d024fb
Nathan <contact@njc.digital>
2023-05-28 16:15:10 +0100

    debug list-directories.sh

--- aws-stack/list-directories.sh
+++ aws-stack/list-directories.sh
@@ -4,4 +4,5 @@
 directories=$(find . -type d -not -path '*/\.*' -not -path '.' -not -path './.git*' -not -path './pipeline-scripts*' -mindepth 2 -maxdepth 2)
 
 # Output a space-separated list
-echo "directories=\"$(echo $directories | tr '\n' ' ')\"" | tee $GITHUB_OUTPUT
+# echo "directories=\"$(echo $directories | tr '\n' ' ')\"" | tee $GITHUB_OUTPUT
+echo "directories=$directories" | tee $GITHUB_OUTPUT
