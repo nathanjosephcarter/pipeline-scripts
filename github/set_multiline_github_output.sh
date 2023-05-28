30d74be376457be53e74572b21c589afd7f8a05d
Nathan <contact@njc.digital>
2023-05-28 18:50:51 +0100

    debug list-directories.sh

--- github/set_multiline_github_output.sh
+++ github/set_multiline_github_output.sh
@@ -12,6 +12,6 @@ set_multiline_github_output() {
   done
 
   echo "${output_name_variable}<<EOF" >> "$GITHUB_OUTPUT"
-  echo "${!output_value_variable}" >> "$GITHUB_OUTPUT"
+  echo "${output_value_variable}" >> "$GITHUB_OUTPUT"
   echo 'EOF' >> "$GITHUB_OUTPUT"
 }
