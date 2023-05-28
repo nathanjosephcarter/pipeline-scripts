797f4bb836c220c2e665d385b25f34fe32c3ef46
Nathan <contact@njc.digital>
2023-05-28 15:11:38 +0100

    debug list-directories.sh

--- aws-stack/list-directories.sh
+++ aws-stack/list-directories.sh
@@ -4,4 +4,4 @@
 directories=$(find . -type d -not -path '*/\.*' -not -path '.' -not -path './.git*' -mindepth 2 -maxdepth 2)
 
 # Output the directories as JSON
-jq -cr '@json' $directories | tee $GITHUB_OUTPUT
+jq -cr '@json' "$directories" | tee $GITHUB_OUTPUT
