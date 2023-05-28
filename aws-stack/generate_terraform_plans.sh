74379c61a70f01e22f26df438b4cc8912f90c0ee
Nathan <contact@njc.digital>
2023-05-28 22:49:37 +0100

    debug generate_terraform_plans.sh

--- aws-stack/generate_terraform_plans.sh
+++ aws-stack/generate_terraform_plans.sh
@@ -3,12 +3,12 @@
 # Create the plan_outputs directory (if it doesn't exist)
 mkdir -p plan_outputs
 
-# Set the IFS variable to split on newline characters
-IFS=$'\n'
+# Read the multiline value of DIRECTORIES into an array
+IFS=$'\n' read -r -d '' -a modified_directories <<< "$MODIFIED_DIRECTORIES"
 
 # Iterate over each directory in the DIRECTORIES variable
 
-for dir in "${MODIFIED_DIRECTORIES[@]}"; do
+for dir in "${modified_directories[@]}"; do
   cd "$dir" # Change to the directory
 
   # Create the parent directories for the plan output file
@@ -18,7 +18,9 @@ for dir in "${MODIFIED_DIRECTORIES[@]}"; do
   terraform init
 
   # Perform Terraform plan and save the output to the plan_outputs directory
-  output_path="../plan_outputs/${dir//\//-}.tfplan"
+  sanitized_dir="${dir//\//-}" # Remove slashes from directory name
+  sanitized_dir="${sanitized_dir#.}" # Remove leading dot from directory name
+  output_path="../plan_outputs/$sanitized_dir.tfplan"
   echo "Writing plan file $output_path"
   terraform plan -out "$output_path"
 
