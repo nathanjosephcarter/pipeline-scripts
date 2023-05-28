f324e98a32cb22dfeb2e9504fd1992869032350a
Nathan <contact@njc.digital>
2023-05-28 15:47:30 +0100

    debug list-directories.sh

--- aws-stack/list-directories.sh
+++ aws-stack/list-directories.sh
@@ -2,5 +2,6 @@
 
 # Capture the list of directories
 directories=$(find . -type d -not -path '*/\.*' -not -path '.' -not -path './.git*' -mindepth 2 -maxdepth 2)
+spaceSeparated=$(printf "%s " "${directories[@]}")
 
-echo $directories | tee $GITHUB_OUTPUT
+echo $spaceSeparated | tee $GITHUB_OUTPUT
