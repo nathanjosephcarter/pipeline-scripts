232b4520ed126a81542b1ffde0628dd0e2911d87
Nathan <contact@njc.digital>
2023-05-28 21:40:39 +0100

    add comments to find_modified_directories.sh

--- aws-stack/find_modified_directories.sh
+++ aws-stack/find_modified_directories.sh
@@ -3,12 +3,18 @@
 # Source the output functions file
 source "./pipeline-scripts/github/set_multiline_github_output.sh"
 
+# Read the multiline value of DIRECTORIES into an array
 IFS=$'\n' read -r -d '' -a directories <<< "$DIRECTORIES"
 
+# Fetch the target branch
 git fetch origin "$TARGET_BRANCH"
 
+# Initialize an array to store modified directories
 modified_directories=()
+
+# Iterate over each directory
 for dir in "${directories[@]}"; do
+  # Check for file changes in the directory
   git_diff_output=$(git diff "origin/$TARGET_BRANCH" --name-only -- "$dir")
   if [[ -n "$git_diff_output" ]]; then
     echo "File changes detected in $dir."
@@ -18,4 +24,5 @@ for dir in "${directories[@]}"; do
   fi
 done
 
+# Set the modified directories as a multiline output
 set_multiline_github_output -n "modified_directories" -v "$modified_directories"
