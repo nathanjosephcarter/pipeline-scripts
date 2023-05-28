d55899df73a04e9c50c60f3302418bb695f53e9f
Nathan <contact@njc.digital>
2023-05-28 16:40:47 +0100

    add comments to set_multiline_github_output.sh

--- aws-stack/list-directories.sh
+++ aws-stack/list-directories.sh
@@ -6,6 +6,8 @@ source "../github/set_multiline_github_output.sh"
 # Capture the list of directories
 directories=$(find . -type d -not -path '*/\.*' -not -path '.' -not -path './.git*' -not -path './pipeline-scripts*' -mindepth 2 -maxdepth 2)
 
+# Print the directories
 echo $directories
 
+# Call the function to set the multiline output
 set_multiline_github_output -name "directories" -value $directories
