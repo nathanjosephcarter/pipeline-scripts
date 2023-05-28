46473c62544ca4215b6a85999caac2c4cfd7cf1a
Nathan <contact@njc.digital>
2023-05-28 18:44:57 +0100

    debug list-directories.sh

--- aws-stack/list-directories.sh
+++ aws-stack/list-directories.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-ls ./pipeline-scripts/github
-echo ""
-ls -l ./pipeline-scripts/github/set_multiline_github_output.sh
-
 # Source the output functions file
 source "./pipeline-scripts/github/set_multiline_github_output.sh"
 
@@ -14,4 +10,4 @@ directories=$(find . -type d -not -path '*/\.*' -not -path '.' -not -path './.gi
 echo $directories
 
 # Call the function to set the multiline output
-set_multiline_github_output -name "directories" -value $directories
+set_multiline_github_output -name "directories" -value "$directories"
