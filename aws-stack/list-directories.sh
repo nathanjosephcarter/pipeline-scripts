1e7b9ad876366242c9a8476442c656ccbf6b52d0
Nathan <contact@njc.digital>
2023-05-28 16:39:11 +0100

    debug list-directories.sh

--- aws-stack/list-directories.sh
+++ aws-stack/list-directories.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# Source the output functions file
+source "../github/set_multiline_github_output.sh"
+
 # Capture the list of directories
 directories=$(find . -type d -not -path '*/\.*' -not -path '.' -not -path './.git*' -not -path './pipeline-scripts*' -mindepth 2 -maxdepth 2)
 
 echo $directories
 
-echo 'directories<<EOF' >> $GITHUB_OUTPUT
-echo "$directories" >> $GITHUB_OUTPUT
-echo 'EOF' >> $GITHUB_OUTPUT
+set_multiline_github_output -name "directories" -value $directories
---
+++ github/set_multiline_github_output.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Function to set multiline output for GitHub Actions
+set_multiline_github_output() {
+  while getopts ":name:value:" flag; do
+    case "$flag" in
+      name) output_name_variable=$OPTARG ;;
+      value) output_value_variable=$OPTARG ;;
+      :) echo "Error: Option -$OPTARG requires an argument." >&2 ; exit 1 ;;
+      \?) echo "Error: Invalid option -$OPTARG." >&2 ; exit 1 ;;
+    esac
+  done
+
+  echo "${output_name_variable}<<EOF" >> "$GITHUB_OUTPUT"
+  echo "${!output_value_variable}" >> "$GITHUB_OUTPUT"
+  echo 'EOF' >> "$GITHUB_OUTPUT"
+}
