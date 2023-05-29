068dca5ebbeff0f08a4d5bdc75b7f611ec77c25d
Nathan <contact@njc.digital>
2023-05-29 02:05:51 +0100

    debug update_pr_plan_comments.sh

--- aws-stack/update_pr_plan_comments.sh
+++ aws-stack/update_pr_plan_comments.sh
@@ -6,11 +6,6 @@ source "./pipeline-scripts/aws-stack/sanitise_directory_name.sh"
 # Set IFS to a space character
 IFS=' '
 
-# Debug
-echo "ls ./plan-outputs/"
-ls ./plan-outputs/
-
-
 # Split the space-separated string into an array
 directories=($DIRECTORIES)
 
@@ -21,22 +16,43 @@ for dir in "${directories[@]}"; do
   new_plan_output=$(cat "$output_path")
 
   # Get the previous comment ID for the directory (if exists)
-  comment_id=$(gh pr view $EVENT_NUMBER --json "comments,body,id" -q ".comments[] | select(.body | startswith(\"Terraform Plan Output - $dir\")) | .id")
-  echo "Previous comment ID for $dir: $comment_id"
+  comment_ids=$(gh pr view $EVENT_NUMBER --json "comments,body,id" -q ".comments[] | select(.body | startswith(\"Terraform Plan Output - $dir\")) | .id")
 
-  # Check if the new plan is different from the previous one
-  if [[ -n "$comment_id" ]]; then
-    previous_plan_output=$(gh pr view $EVENT_NUMBER --json "comments,body" -q ".comments[] | select(.id == \"$comment_id\") | .body" | sed 's/^Terraform Plan Output - $dir\n//')
-    if [[ "$new_plan_output" != "$previous_plan_output" ]]; then
-      # Delete the previous comment
-      gh pr comment --delete $comment_id
-      echo "Deleted previous plan output comment for $dir"
-    else
-      echo "No changes in Terraform plan output for $dir. Skipping comment delete."
+  # Check if any comment IDs were found
+  if [[ -n "$comment_ids" ]]; then
+    # Sort the comment IDs in reverse order to keep the most recent one
+    sorted_comment_ids=$(echo "$comment_ids" | sort -r)
+
+    # Get the most recent comment ID
+    most_recent_comment_id=$(echo "$sorted_comment_ids" | head -n 1)
+
+    # Iterate over the sorted comment IDs
+    for id in $sorted_comment_ids; do
+      # Skip the most recent comment ID
+      if [[ $id != $most_recent_comment_id ]]; then
+        # Delete the previous comment
+        gh pr comment --delete $id
+        echo "Deleted previous plan output comment with ID $id for $dir"
+      fi
+    done
+
+    # Check if the new plan is different from the previous one
+    if [[ -n "$most_recent_comment_id" ]]; then
+      previous_plan_output=$(gh pr view $EVENT_NUMBER --json "comments,body" -q ".comments[] | select(.id == \"$most_recent_comment_id\") | .body" | sed 's/^Terraform Plan Output - $dir\n//')
+      if [[ "$new_plan_output" != "$previous_plan_output" ]]; then
+        # Delete the previous comment
+        gh pr comment --delete $most_recent_comment_id
+        echo "Deleted previous plan output comment for $dir"
+        # Add a new comment with the new plan output
+        gh pr comment $EVENT_NUMBER --body "$(echo -e "Terraform Plan Output - $dir\n$new_plan_output")"
+        echo "Added new plan output comment for $dir"
+      else
+        echo "No changes in Terraform plan output for $dir. Skipping comment delete."
+      fi
     fi
+  else
+    # Add a new comment with the new plan output
+    gh pr comment $EVENT_NUMBER --body "$(echo -e "Terraform Plan Output - $dir\n$new_plan_output")"
+    echo "Added new plan output comment for $dir"
   fi
-
-  # Add a new comment with the new plan output
-  gh pr comment $EVENT_NUMBER --body "$(echo -e "Terraform Plan Output - $dir\n$new_plan_output")"
-  echo "Added new plan output comment for $dir"
 done
