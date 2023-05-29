7d2ef1c5d7ed7da0f4723e67bfe11470a4350eef
Nathan <contact@njc.digital>
2023-05-29 14:54:58 +0100

    remove impossible delete_pr_comment.sh script

--- github/delete_pr_comment.sh
+++ github/delete_pr_comment.sh
@@ -1,37 +1,40 @@
-#!/bin/bash
+# When Github allows for PR comments to be deleted this could possible be used
 
-# Function to santise directory name
-delete_pr_comment() {
-  local output_repo_variable
-  local output_comment_variable
 
-  while getopts ":r:c:" flag; do
-    case "$flag" in
-      r) output_repo_variable=$OPTARG ;;
-      c) output_comment_variable=$OPTARG ;;
-      :) echo "Error: Option -$OPTARG requires an argument." >&2 ; exit 1 ;;
-      \?) echo "Error: Invalid option -$OPTARG." >&2 ; exit 1 ;;
-    esac
-  done
+# #!/bin/bash
 
-  if [[ -z $output_repo_variable ]]; then
-    echo "Error: Repo must not be empty." >&2
-    exit 1
-  fi
+# # Function to santise directory name
+# delete_pr_comment() {
+#   local output_repo_variable
+#   local output_comment_variable
 
-  if [[ -z $output_comment_variable ]]; then
-    echo "Error: Comment ID must not be empty." >&2
-    exit 1
-  fi
+#   while getopts ":r:c:" flag; do
+#     case "$flag" in
+#       r) output_repo_variable=$OPTARG ;;
+#       c) output_comment_variable=$OPTARG ;;
+#       :) echo "Error: Option -$OPTARG requires an argument." >&2 ; exit 1 ;;
+#       \?) echo "Error: Invalid option -$OPTARG." >&2 ; exit 1 ;;
+#     esac
+#   done
 
-  endpoint="/repos/nathanjosephcarter/$output_repo_variable/pulls/comments/$output_comment_variable"
-  echo "Endpoint:"
-  echo $endpoint
+#   if [[ -z $output_repo_variable ]]; then
+#     echo "Error: Repo must not be empty." >&2
+#     exit 1
+#   fi
 
-  gh api \
-    --method DELETE \
-    -H "Accept: application/vnd.github+json" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    $endpoint
-}
+#   if [[ -z $output_comment_variable ]]; then
+#     echo "Error: Comment ID must not be empty." >&2
+#     exit 1
+#   fi
+
+#   endpoint="/repos/nathanjosephcarter/$output_repo_variable/pulls/comments/$output_comment_variable"
+#   echo "Endpoint:"
+#   echo $endpoint
+
+#   gh api \
+#     --method DELETE \ # this is incorrect - deletes a PR Review comment rather than a PR comment
+#     -H "Accept: application/vnd.github+json" \
+#     -H "X-GitHub-Api-Version: 2022-11-28" \
+#     $endpoint
+# }
 
