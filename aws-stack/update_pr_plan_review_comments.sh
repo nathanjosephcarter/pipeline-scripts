a74b86d1ed6dae7ed7cb9e8de4e3fd5c6d148676
Nathan <contact@njc.digital>
2023-05-29 15:36:12 +0100

    add review comments script

---
+++ aws-stack/update_pr_plan_review_comments.sh
@@ -0,0 +1,5 @@
+reviews=(gh api \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "/repos/nathanjosephcarter/$REPO/pulls/$PULL_NUMBER/reviews")
+echo $reviews
