a6e8da66365eb3e9ca686dbcf0708e0abfc7165d
Nathan <contact@njc.digital>
2023-05-29 15:43:46 +0100

    debug plan review comments script

--- aws-stack/update_pr_plan_review_comments.sh
+++ aws-stack/update_pr_plan_review_comments.sh
@@ -1,5 +1,5 @@
 gh --version
-reviews=(gh api \
+reviews=("gh api \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "/repos/nathanjosephcarter/$REPO/pulls/$PULL_NUMBER/reviews")
